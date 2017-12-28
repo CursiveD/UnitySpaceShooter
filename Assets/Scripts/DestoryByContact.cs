34d59927c333a1ee0b3a784bb6c0e4652a240b0b
Darren M. OConnell <doconnell@cursived.com>
2017-12-28 03:49:41 -0800

    Unity Space Shooter Starter Project The Completed Version Of The Space Shooter Unity Starter Project. Major code differences between this and the tutorial provided by unity. Signed-off-by: Darren M. OConnell <doconnell@cursived.com>

---
+++ Assets/Scripts/DestoryByContact.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryByContact : MonoBehaviour 
+{
+
+	private GameController gc; 
+	public GameObject explosion;
+	public GameObject playerExplosion;
+	public int scoreValue;
+
+	void Start()
+	{
+		GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+		if (gameControllerObject != null)
+		{
+			gc = gameControllerObject.GetComponent<GameController>();
+		}
+		if(gc == null)
+		{
+			Debug.Log("Cannot find 'GameController' script");
+
+		}
+	}
+	
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "Boundary") {
+			return;
+		} 
+		else {
+			
+			Instantiate (explosion, transform.position, transform.rotation);
+			if (other.tag == "Player") {
+				Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+				gc.GameOver();
+				gc.AddScore(-scoreValue);
+				
+			} 
+			
+				Destroy (other.gameObject);
+				Destroy (gameObject);
+				gc.AddScore (scoreValue);
+			
+		
+		}
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour 
+{
+	public GameObject hazard;
+	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait = 0;
+	public float startWait = 0;
+	public float waveWait = 0;
+	
+	public UnityEngine.UI.Text scoreText;
+	public UnityEngine.UI.Text restartText; 
+	public UnityEngine.UI.Text gameOverText;
+	private int score =0 ;
+	
+	private bool gameOver = false;
+	private bool restart = false;
+	void Start ()
+	{	
+		UpdateScore();
+		StartCoroutine(SpawnWaves());
+		//restartText.text = gameOverText.text = "";
+		restartText.text = "";
+		gameOverText.text = "";
+
+	}	
+
+	void Update()
+	{
+		if(this.restart && Input.GetKeyDown (KeyCode.R))
+		{
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);	
+		
+			
+	    }
+	}
+	IEnumerator SpawnWaves()
+	{
+		yield return new WaitForSeconds(startWait);
+		while(gameOver == false)
+		{
+			for(int i = 0; i< hazardCount; i++)
+			{
+				Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x),spawnValues.y,spawnValues.z);
+				Quaternion spawnRoation = Quaternion.identity;
+				Instantiate(hazard, spawnPosition, spawnRoation);
+				yield return new WaitForSeconds(spawnWait);
+				//Debug.Log("End Wave");
+				
+			}
+			
+			yield return new WaitForSeconds(waveWait);
+			
+		}
+		if(gameOver == true)
+			{
+				restartText.text = "Press 'R' for Restart";
+				restart = true;
+				
+			}
+	}
+	
+	public void AddScore (int newScoreValue)
+	{
+		score += newScoreValue;
+		UpdateScore();
+
+	}
+	public void GameOver()
+	{
+		gameOverText.text = "Game Over!";
+		gameOver = true;
+		//Debug.Log("Game Over");
+	}
+	void UpdateScore()
+	{
+		scoreText.text = "Score: " + score;
+	}
+
+	
+
+	
+}
